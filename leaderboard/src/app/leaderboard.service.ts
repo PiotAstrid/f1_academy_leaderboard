924d23e761ff381cb1673c67607ad056359e4caa
Astrid Piot <astrid.piot@icloud.com>
2025-05-02 16:06:01 +0200

    sorted leaderboard

--- leaderboard/src/app/leaderboard.service.ts
+++ leaderboard/src/app/leaderboard.service.ts
@@ -18,17 +18,37 @@ export class LeaderboardService {
   constructor() { }
 
   getAllLeaderBoardEntries(): Observable<LeaderboardEntry[]>{
-    // TODO return sorted list
     return this.entriesSubject.asObservable();
   }
 
   // add a new entry to the leaderboard
   addEntry(name: string, color: string, time: string){
     if (name && time){
+      // the default color is black
       if (!color) color = '#000000';
       this.leaderboardEntryList = [...this.leaderboardEntryList, { name, color, time }];
-      this.entriesSubject.next(this.leaderboardEntryList);
+      
+      // sort list by time
+      const sorted = [...this.leaderboardEntryList].sort((a, b) =>
+        this.timeToMilliseconds(a.time) - this.timeToMilliseconds(b.time)
+      );
+      
+      this.entriesSubject.next(sorted);
     }
+  }
+
+  private timeToMilliseconds(time: string): number {
+    // pattern: m:ss:mmm or 1:23:456
+    const match = time.match(/^(\d+):(\d{2})\.(\d{3})$/) 
+    // if time is invalid, return infinity
+    if (!match) return Infinity; 
+
+    const minutes = parseInt(match[1], 10);
+    const seconds = parseInt(match[2], 10);
+    const milliseconds = parseInt(match[3], 10);
+
+    return minutes * 60000 + seconds * 1000 + milliseconds;
+
   }
 
 }
--- leaderboard/src/app/leaderboard/leaderboard.component.ts
+++ leaderboard/src/app/leaderboard/leaderboard.component.ts
@@ -21,5 +21,20 @@ export class LeaderboardComponent {
     this.entries$ = this.leaderboardService.getAllLeaderBoardEntries();
   }
 
+  getTextColorClass(color: string): string {
+    return this.isColorDark(color) ? 'text-white' : 'text-black';
+  }
+  
+  private isColorDark(color: string): boolean {
+    // convert to rgb
+    const hex = color.replace('#', '');
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+  
+    // Calculate luminance
+    const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+    return luminance < 0.5; // true if dark, false if light
+  }
 
 }
