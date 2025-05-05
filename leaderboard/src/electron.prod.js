8a3dfe0a172f0b0cc0569f3c62151565275796f6
Astrid Piot <astrid.piot@icloud.com>
2025-05-05 17:07:12 +0200

    electron changes

--- leaderboard/src/electron.prod.js
+++ leaderboard/src/electron.prod.js
@@ -9,8 +9,8 @@ let win;
 const createWindow = () => {
     // Create the browser window.
     win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1280,
+        height: 1024,
         icon: path.join(__dirname, 'favicon.ico'),
     });
 
